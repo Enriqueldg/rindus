013fede311224bc7bfb5634370685cb4baf345c8
kike <enriqueldg@gmail.com>
2019-08-06 21:05:02 +0100

    adding a RegexValidator on iban field

--- client_management/models.py
+++ client_management/models.py
@@ -2,12 +2,13 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class Client(models.Model):
     first_name = models.CharField(max_length=15, null=False)
     last_name = models.CharField(max_length=35, null=False)
-    iban = models.CharField(max_length=24, null=False)
+    iban = models.CharField(max_length=24, null=False, validators=[RegexValidator('[A-Z][A-Z]([0-9]*$)')])
     created_by = models.EmailField()
 
     class Meta:
