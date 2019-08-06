336056da6ad485f71df58ee955754776ae4d5636
kike <enriqueldg@gmail.com>
2019-08-06 18:38:02 +0100

    creating client model

--- client_management/models.py
+++ client_management/models.py
@@ -3,4 +3,15 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=15, null=False)
+    last_name = models.CharField(max_length=35, null=False)
+    iban = models.CharField(max_length=24, null=False)
+    created_by = models.EmailField()
+
+    class Meta:
+        db_table = "client"
+
+    def __str__(self):
+        return "{0} {1}".format(self.first_name, self.last_name, self.iban)
