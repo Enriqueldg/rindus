ac88863276e8120c049048b2892568b180f8a7dd
kike <enriqueldg@gmail.com>
2019-08-06 18:40:04 +0100

    setting postgresql database and making initial migration

--- rindus/settings.py
+++ rindus/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'rindus.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'rindusdb',
+        'USER': 'postgres',
+        'PASSWORD': '123456',
+        'HOST': '/var/run/postgresql',
+        'PORT': '5432',
     }
 }
 
